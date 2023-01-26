ace516e2a642265ad94e4b50fef6082d2b6850e4
YoungWoong22 <hwa8078678@naver.com>
2023-01-26 20:06:05 +0900

    js수업 시작 다시 수업 조금씩듣는중 자소서 및 취업준비, 3월에 있을 정처기 공부중

---
+++ section14-javaScript기초/index2.js
@@ -0,0 +1,11 @@
+// Let's get some practice defining constants. In index.js, please define the following two variables:
+
+// Define a constant called boilingPointC and set it to 100. 
+
+// Define a second constant called boilingPointF and set it to 212. 
+
+// That's it!
+
+// YOU CODE GOES HERE:
+const boilingPointC = 100;
+const boilingPointF = 212;
