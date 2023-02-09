d3e7cc31f7bbab9730231021c69c2aa1d66c7036
YoungWoong22 <hwa8078678@naver.com>
2023-02-09 15:42:16 +0900

    no message

---
+++ section22-콜백/index1.js
@@ -0,0 +1,24 @@
+// console.log("Hello!!!....")
+// setTimeout(() => {
+//     console.log("....are you still there?")
+// },3000)
+
+// console.log("Good Bye");
+
+
+
+
+// setInterval(() => {
+//     console.log(Math.random())
+// },2000);
+
+// clearInterval(id);
+
+
+// const exams = [80,98,92,78,70,90,89,84,81,77]
+
+// exams.every(score => score >= 75)
+
+// exams.some(exams => seams > 80)
+
+
